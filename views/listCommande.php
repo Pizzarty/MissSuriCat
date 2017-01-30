2b184fa27fcec6d1427e3ce11c9e332f85906807
Elisa KLEIN <Elisa KLEIN>
2017-01-30 15:26:05 +0100

    liste commande

---
+++ views/listCommande.php
@@ -0,0 +1,27 @@
+<a href="./index.php?action=formAddCommande">Ajouter</a>
+<table>
+  <thead>
+    <th>Id Commande</th>
+    <th>Référence</th>
+    <th>Nom Client</th>
+    <th>Prénom Client</th>
+    <th>Date Commande</th>
+    <th>Date Expédition</th>
+    <th>Statut Commande</th>
+  </thead>
+  <tbody>
+    <?php
+      foreach ($listeCommande as $commande) {
+        echo '<tr>';
+          echo '<td>' . $commande->getId() . '</td>';
+          echo '<td>' . $commande->getRef() . '</td>';
+          echo '<td>' . $commande->getNom() . '</td>';
+          echo '<td>' . $commande->getPrenom() . '</td>';
+          echo '<td>' . $commande->getDateCommande() . '</td>';
+          echo '<td>' . $commande->getDateExpedition() . '</td>';
+          echo '<td>' . $commande->getStatut() . '</td>';
+        echo '</tr>';
+      }
+    ?>
+  </tbody>
+</table>
