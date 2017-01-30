e525f0f803352d3779608d7c0cdc0d89eef372f3
Elisa KLEIN <Elisa KLEIN>
2017-01-30 15:06:49 +0100

    layout

--- layouts/layout.php
+++ layouts/layout.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta charset="utf-8">
     <title>Blog Miss SuriCat</title>
   </head>
   <body>
+    <?php if (!empty($_SESSION['login'])) { ?>
+      <!-- Bloc Menu -->
+      <a href="./index.php?action=listClient">Clients</a>
+      <a href="./index.php?action=listCommande">Commandes</a>
+      <label>Bienvenue sur mon blog de la mort qui tue <?php echo $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ?></label>
+      <a href="./index.php?action=disconnect">Déconnexion</a>
+    <?php } ?>
+    <!-- Bloc Contenu -->
     <div id="contenu">
       <!-- Affiche une vue en fonction de l'action que l'utilisateur souhaite réaliser -->
       <?php include($vueAAfficher); ?>
