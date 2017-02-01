0756d85410f9a4f960459efde21c3b487be09d56
Eliiisa <elisaklein66@gmail.com>
2017-02-01 13:52:40 +0100

    menu avec produit

--- layouts/layout.php
+++ layouts/layout.php
@@ -9,6 +9,7 @@
       <!-- Bloc Menu -->
       <a href="./index.php?action=listClient">Clients</a>
       <a href="./index.php?action=listCommande">Commandes</a>
+      <a href="./index.php?action=listProduit">Produits</a>
       <label>Bienvenue sur mon blog de la mort qui tue <?php echo $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ?></label>
       <a href="./index.php?action=disconnect">Déconnexion</a>
     <?php } ?>
