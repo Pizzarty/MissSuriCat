8217ea3d300927ead5315e07126dec5df931a3ab
Pierre Banville <Pierre Banville>
2017-02-01 11:40:36 +0100

    Ajout statut

--- models/repositories/CommandeRepository.php
+++ models/repositories/CommandeRepository.php
@@ -5,7 +5,7 @@
 
     public function getAll($pdo){
 
-      $req = $pdo->query("SELECT p.nom, p.prenom, cmd.id, cmd.ref, cmd.date_expedition, cmd.date_cmd, s.libelle
+      $req = $pdo->query("SELECT p.civilite, p.nom, p.prenom, cmd.id, cmd.ref, cmd.date_expedition, cmd.date_cmd, s.libelle
       FROM personne p INNER JOIN commande cmd ON p.id=cmd.client_id INNER JOIN statut s ON cmd.statut_id=s.id ORDER BY p.nom, p.prenom");
 
       $req->setFetchMode(PDO::FETCH_OBJ);
@@ -16,17 +16,20 @@
       while ($obj = $req->fetch()){
 
         $client = new Client();
+        $client->setCivilite($obj->civilite);
         $client->setNom($obj->nom);
         $client->setPrenom($obj->prenom);
-        
+
+        $statut = new Statut();
+        $statut->setLibelle($obj->libelle);
 
         $commande = new Commande();
         $commande->setId($obj->id);
         $commande->setReference($obj->ref);
         $commande->setClient($client);
+        $commande->setStatut($statut);
         $commande->setDateCommande($obj->date_cmd);
         $commande->setDateExpedition($obj->date_expedition);
-        $commande->setStatut($obj->libelle);
 
         $listCommande[] = $commande;
 
