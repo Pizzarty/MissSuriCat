477174b543a97453ddccb86455bd664c2b10b023
Eliiisa <elisaklein66@gmail.com>
2017-02-16 11:21:31 +0100

    form passer commande

--- views/passerCommande.php
+++ views/passerCommande.php
@@ -1,17 +1,22 @@
 <form action="./index.php" method="POST">
 
+<?php foreach ($listProduit as $produit ) { ?>
   <label>Produit</label>
   <select name="produit" value="">
+    <option> </option>
     <?php foreach ($listProduit as $produit ) { ?>
     <option value=<?php echo $produit->getId()?>>
       <?php
         echo $produit->getLibelle();
       ?>
     </option>
+    <br>
     <?php } ?>
   </select>
 <label>Quantité</label>
-<input type="number" name="quantité" value="1">
+<input type="number" name="quantité" value="0">
+<br>
+    <?php } ?>
   <input type="submit" value="Ajouter au panier"/>
   <br>
   <label><?php echo $message ?></label>
